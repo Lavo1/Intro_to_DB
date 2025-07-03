23ab4994399b4f78647224002e649b1aa970b575
Lavo1 <leratolerama3@gmail.com>
2025-07-03 13:48:41 +0200

    Create task_4.sql

---
+++ task_4.sql
@@ -0,0 +1,14 @@
+-- Print full description of the table 'books' without using DESCRIBE or EXPLAIN
+
+SELECT
+    COLUMN_NAME,
+    COLUMN_TYPE,
+    IS_NULLABLE,
+    COLUMN_KEY,
+    COLUMN_DEFAULT,
+    EXTRA
+FROM
+    INFORMATION_SCHEMA.COLUMNS
+WHERE
+    TABLE_SCHEMA = DATABASE()
+    AND TABLE_NAME = 'Books';
